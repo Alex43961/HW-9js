9674e6b9a16c3a8a9422fcf18eeed7491efb924d
Alex43961 <semen43961@gmail.com>
2023-03-05 01:38:18 +0200

    first solution

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict"
-// 1) If even one of group have a ligher return "good sheet"
- // 2) Sum of age into the group
- // 3) Divide group on three groups : "Who knows JS" , "Who dont's know JS" , "Who knows CSS"
+// 2) Sum of age into the group
+// 1) If even one of group have a ligther return "good sheet"
+// 3) Divide group on three groups : "Who knows JS" , "Who dont's know JS" , "Who knows CSS"
 const group = [
 	{
 	  firstName: "John",
@@ -31,6 +31,65 @@ const group = [
 	  },
 	},
  ];
- 
- 
- let isLigher = function (group, [pocket])
+
+ group[0].pocket.left.concat(group[0].pocket.right)
+
+ group.includes('includes')
+
+
+
+//let isLighter = group.filter(function(elem, i){
+
+//	return elem.pocket[i] === "lighter"
+
+// });
+// console.log(isLighter);
+//let cities = [{ id: 121, name: 'г. Урюпинск' }, { id: 122, name: 'г. Париж' }, { id: 123, name: 'г. Москва' }, { id: 124, name: 'г. Штормград' }];
+//let searchTerm = 'г. Москва';
+//let cityId = cities.find(city => city.name === searchTerm).id
+//console.log(cityId);
+//let isLighter = group.find(group => group.name === "lighter").pocket[]
+//console.log(isLighter);
+//!let x = "lighter"
+//function isLighter(str){
+//	return str === x;
+//}
+//console.log(group.some(isLighter));
+//let NewGroup = group.flat(Infinity);
+//console.log(NewGroup);
+//let isFind = NewGroup.filter(function(val){
+//	return "lighter";
+//});
+//console.log(isFind);
+//console.log(group.indexOf("lighter"));
+//let found = group.find(e =>e.name === "lighter");
+//console.log(found);
+
+let sumAge = group.reduce(
+	(accumulator, currentValue) => accumulator + currentValue.age,
+	0
+);
+
+
+let age = 0;
+for(let i = 0; i < group.length; i++) {
+	return age += group[i].age;
+}
+
+group.forEach(element => age += element.age);
+group.forEach((element) => {
+	return age += element.age
+});
+
+group.map(element => age += element.age)
+
+(a,b, c) => {
+	//
+	return a+b+c;
+}
+
+function (a,b,c) {
+	//
+	this
+	return a+b+c;
+}
